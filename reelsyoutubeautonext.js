2e09725c90ef659b1ce0bfa3b6c982245c1a2915
Cesar Velasquez <58951446+underline-software@users.noreply.github.com>
2024-06-07 21:52:11 -0400

    Create reelsyoutubeautonext.js

---
+++ reelsyoutubeautonext.js
@@ -0,0 +1,25 @@
+const ClickManager = {
+    //Debes salir del primer video y pegar este objeto en la consola y darle enter. Luego debes iniciar con ClickManager.start, eso hará que cambie cada 15 a 20 segundos.
+    intervalID: null,
+    start: function() {
+        const clickButton = () => {
+            try {
+                document.getElementsByClassName("yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-xl yt-spec-button-shape-next--icon-button")[1].click();
+                console.log("Botón clicado.");
+            } catch (error) {
+                console.error("No se pudo clicar el botón:", error);
+            }
+        };
+        this.intervalID = setInterval(clickButton, 20000);
+        console.log("Ejecución iniciada.");
+    },
+    stop: function() {
+        if (this.intervalID !== null) {
+            clearInterval(this.intervalID);
+            this.intervalID = null;
+            console.log("La ejecución ha sido cancelada.");
+        } else {
+            console.log("No hay ninguna ejecución en curso para cancelar.");
+        }
+    }
+};
